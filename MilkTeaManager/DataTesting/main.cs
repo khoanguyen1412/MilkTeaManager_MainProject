fc31ef0976d306b0be5a4b567192ef7e65126e3d
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2019-12-05 13:22:37 +0700

    Data Add

---
+++ MilkTeaManager/DataTesting/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MilkTeaManager.Models;
+namespace DataTesting
+{
+	 public class main
+	{
+
+		static void Main()
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			foreach (var item in DataAccess.GetSanphams())
+			{
+				Console.WriteLine(item.TENSP);
+			}
+
+			Console.Read();
+		}
+	}
+}
