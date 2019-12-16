a322982164365f0ba5b4a10174eeb48a89bdba1a
phamphuckhai <34903119+phamphuckhai@users.noreply.github.com>
2019-12-17 02:43:27 +0700

    Fix Miscellaneous

---
+++ MilkTeaManager/MilkTeaManager/ViewModels/DelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MilkTeaManager.ViewModels
+{
+    internal class DelegateCommand : ICommand
+    {
+        private object doSomething;
+        private object canDoSomething;
+
+        public DelegateCommand(object doSomething, object canDoSomething)
+        {
+            this.doSomething = doSomething;
+            this.canDoSomething = canDoSomething;
+        }
+
+        public DelegateCommand(Action<object> doSomething, Func<object, bool> canDoSomething)
+        {
+            this.doSomething = doSomething;
+            this.canDoSomething = canDoSomething;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MilkTeaManager/MilkTeaManager/ViewModels/MainHomepageViewModel.cs
+++ MilkTeaManager/MilkTeaManager/ViewModels/MainHomepageViewModel.cs
@@ -14,12 +14,65 @@ namespace MilkTeaManager.ViewModels
     {
 
         private string _hKey;
-        public ICommand LoadKhachHangPageCommand { get; set; }
-        public ICommand LoadedWindowsCommnad { get; set; }
-        public string HKey { get => _hKey; set => _hKey = value; }
 
+        private readonly ICommand disableButton;
+
+        public ICommand LoadKhachHangPageCommand { get; set; }
+        private bool _disableBH;
+        private bool _disableQL;
+        private bool _disableTK;
+        public ICommand LoadedWindowsCommnad { get; set; }
+        public string HKey { get => _hKey;
+            set {
+                _hKey = value;
+                if (HKey == "1")
+                    DisableBH = true;
+                if (HKey == "0")
+                {
+                    DisableBH = true;
+                    DisableQL = true;
+                    DisableTK = true;
+                }
+                if (HKey == "2")
+                {
+                    DisableQL = true;
+                    DisableTK = true;
+                }
+            }
+        }
+        public ICommand DisableButton
+        {
+            get { return this.disableButton; }
+        }
+
+        public bool DisableBH
+        {
+            get => _disableBH;
+            set
+            {
+                _disableBH = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool DisableQL { get => _disableQL;
+            set {
+                _disableQL = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool DisableTK { get => _disableTK;
+            set {
+                _disableTK = value;
+                OnPropertyChanged();
+            }
+        }
         public MainHomepageViewModel()
         {
+           
+            this.disableButton = new DelegateCommand(this.DoSomething, this.CanDoSomething);
+
+
 
             LoadKhachHangPageCommand = new RelayCommand<object>((p) =>
             {
@@ -28,6 +81,7 @@ namespace MilkTeaManager.ViewModels
             }, (p) =>
             {
                 MessageBox.Show("main");
+                
             });
             LoadedWindowsCommnad = new RelayCommand<Window>((p) =>
             {
@@ -55,6 +109,16 @@ namespace MilkTeaManager.ViewModels
                 }
 
             });
+        }
+        private void DoSomething(object state)
+        {
+            // do something here
+        }
+
+        private bool CanDoSomething(object state)
+        {
+            // return true/false here is enabled/disable button
+            return true;
         }
     }
 }
